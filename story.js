cedb84fd78e59cce90ab71118e12da56200b260d
MAKINDE IFEOLUWA <594756@cart.ads>
2019-10-25 13:12:31 -0700

    added story.js

---
+++ story.js
@@ -0,0 +1,39 @@
+const name = prompt(`Hello there! Welcome to the world of pokémon! My name is Oak!
+ People call me the pokémon Prof! This world is inhabited by creatures called pokémon! 
+ For some people, pokémon are pets. Others use them for fights. Myself...I study pokémon as a profession. Are you a boy or a girl?! 
+ Ummmm... i guess in this day and age we cant ask these types of questions! Atleast i can ask this question!
+ what is you name?`);
+// askes their name so we can us it in code
+
+
+ let story = {
+
+   intro: {
+        text : 'You take a step off the scale. You have gained a lot of weight. You decide to start walking every day to lose weight. You walk out and decide which street you want to take. ',
+        choices : ['Eternity st.', 'Hughes st.', `L's st.`]
+    },
+    one: {
+        text : ' you come a cross a problem ',
+        choices : [2,4,6]
+    },
+    two: {
+        text : ' something happens at the end.',
+        choices : 
+    }
+};
+promptSteet1 = promt(`what are you going to do?`)
+function whereAreYougoing(){
+    if(promptSteet1 == intro.choices || "1" ){
+        promt(`${intro.text}`);
+
+    }else if(){
+
+    }
+    else if(){
+
+    }
+}
+
+
+
+ 
