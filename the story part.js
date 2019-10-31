750133c1813bcb9774a5f7d85fc4bb269bdfae52
MAKINDE IFEOLUWA <594756@cart.ads>
2019-10-31 13:10:13 -0700

    that on time

---
+++ the story part.js
@@ -0,0 +1,107 @@
+let story = {
+    intro : {
+        text : `You take a step off the scale. You have gained a lot of weight. 
+        You decide to do some exersise in the form of walking every day. 
+        You walk out and decide which street you want to take.`,
+        choices : [
+            ["Eternity", "street1"],
+            ["Hughes", "street2"],
+            ["L's", "street3"],
+            ["Lelouch ", "street4"]
+        ]
+    },
+    street1 : {
+        text : `You are aproached by a prophet of Fastism an strange creature that offers 
+        you a once in a melenia chance to convert to Fastism and assend to godhood`,
+        choices : [
+            ["Accept his offer", "offer1"],
+            ["Decline his offer", "offer2"]
+        ]
+    },
+    offer1 : {
+        text : `You accept his request and feel an endless amount of power flow through you.
+        It feels as if your wisdom is as boundless as space itself. You ascend to the sky having achived godhood. 
+        you take your rightfull place as a new god and spend eternity folowing the elder deity`
+    },
+    offer2 : {
+        text : `The last thing you remember 
+        is the grusome nature of your death. 
+        You awaken in a dark void anfd you hear a man say 
+        "Looks like you're going to the shadow realm jimbo"`
+    },
+    street2 : {
+        text:`A wild Chaka Chooka has apeared. 
+        A powerfull beast, known around the world for taking down 
+        entiresky scrapers in a single blow. You have no chance of escaping,
+         so you prepare your first attack. what do you do`,
+        choices : [
+            ["Power of Friendship","powerup1"],
+            ["Detroit Smash","powerup2"],
+            ["Poke-Ball", "powerup3"]
+        ]
+    },
+    powerup1: {
+        text:`You prepare your strongest attack. You remember all the people who have
+         helped you get to where you are and prepare to fight and win in their name.
+        You let out a defiant roar as you remember no one likes you and you have no friends, 
+        this attack fails miserably. The Chaka Chooka is soo sad about meeting such a pathetic 
+        creature that it takes pity on you and becomes your new companion. You wake up to a negotiation
+        between rednecks led by wendy and clowns led by ronald. Since you had your Burger 
+        king T-shirt that day you are offered up by the two armies as a peace offering. what do you do?`, 
+        choices : [
+            ["Join Wendys", "wendys"],
+            ["Hail to the burger king", "offer2"],
+            ["Join Mcdonalds","Mcdonalds"]
+        ]
+    },
+    powerup2: {
+        text:`You feel one for all surge through your arm and prepare to fire of a punch. 
+        You clench your buttcheeks as tight as you can and scream this from the depth of your heart
+        "Detroito Smaaasheee". The smash is effective in  killing the Chaka Chooka.
+        However in your panicked fervor you had accidentally used a 1,000,000% smash and accidentally kill thousands of people. Your Large scale attack did billions of dollars of 
+        property damage and killed hundreds of  people. you are now at the UN bieng trialed for the numerous war crimes you have commited`,
+        choices : [
+            ["Plead the 5th","plead"],
+            ["War crimes 2: Electric bogaloo","bogaloo"]
+        ]
+    },
+    powerup3: {
+        text: `You pull a poke-ball out of your trashy SAO jacket poket and wind up to throw. 
+        You taste the adrenaline in your mouth as your flimsy sphagetti arm hurls the ball at the monster with miniscule force. 
+        In a  flash of briliant white light the chaka chooka disapears. behind him you hear a massive brawl. 
+        people numbering in the hundredes brutally killing each other. you walk over and are knocked unconcious`,
+    },
+    wendys : {
+        text:`Wendys accepts you into the family. The fight continues. 
+        if you wanted more tough luck try an other story line if you dare.`
+    },
+    Mcdonalds: {
+        text: `mcdonalds accept you into the family. The fight continues. 
+        if you wanted more tough luck try an other story line if you dare.`
+    },
+    Plead : {
+        text: `The UN is appaled at your blatant disregard for human life and unimpressed at your understanding of international law. 
+        In the name of the many casualties you have caused`
+
+    },
+    bogaloo : {
+        text: `you prepare another attack. 
+        the people at the trial realize that they have made a horrible mistake. 
+        The attack lands and everyone in your imediete vcicinity ha been killed.`
+    },
+    street3 : {
+        text:`As you walk down the street you see a massive battle off in the distance. You walk closer to see what it is about. You see an army of  pschopathic clowns led by Ronald Mcdonald and another army of inbred hillbillys led by Wendy. Unfortunatley for you had your burger king t-shirt on that day. Both factions stop and notice you. As if they had formed an unspoken truce they begin to attack you.`,
+        choices : [
+            ["Ora Ora Ora",""],
+            [,],
+            [,]
+        ]
+    },
+    street4 : {
+        text:`you see a woman obliviously walking across the street. To her left a truck barelling towards her.`,
+        choices : [
+            [,],
+            [,]
+        ]
+    },
+}
